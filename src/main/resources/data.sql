89a167b835102dd124cf38628365ba31f7c13917
Daniel Sanchez <djsanchez@soaint-ve.com>
2019-08-01 09:52:30 -0400

    Subiendo Proyecto Angular & Spring 5 Creando web app full stack (Angular 8+)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andres', 'Guzman', 'Guzman@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jose', 'Solano', 'Jose@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Felipe', 'Ramirez', 'Felipe@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Daniel', 'Sanchez', 'Daniel@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Tomas', 'Turbado', 'Tomas@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Marcos', 'Salas', 'Marcos@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mercy', 'Torres', 'Mercy@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Alfonzo', 'Gonzalez', 'Alfonzo@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Geomar', 'De Sanchez', 'Geomar@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Luis', 'Vegas', 'Luis@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Miguel', 'Guzman', 'Miguel@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Carlos', 'Tovar', 'Carlos@bolsadeideas.com', '2018-01-01');
